31284f5e02bb9e040deab4d8ad5765579f7a7020
Noah Kim <noah.hw.kim@gmail.com>
2024-11-20 18:37:39 -0800

    removed comments

--- forky.c
+++ forky.c
@@ -3,7 +3,8 @@
 void pattern_one_child_process(int processNum) {
     int sleep_time = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
 
-    printf("Process %d (pid %d) created: will sleep for %d seconds \n", processNum, getpid(), sleep_time);
+    printf("Process %d (pid %d) created: will sleep for %d seconds \n",
+           processNum, getpid(), sleep_time);
 
     sleep(sleep_time);
 
@@ -37,7 +38,9 @@ void pattern_two_child_process(int processNum, int numOfProcesses) {
     int sleep_time = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
 
     if (processNum >= numOfProcesses) {
-        printf("Child %d (pid %d), [no children created] sleeping %d seconds \n", processNum, getpid(), sleep_time);
+        printf(
+            "Child %d (pid %d), [no children created] sleeping %d seconds \n",
+            processNum, getpid(), sleep_time);
         sleep(sleep_time);
         printf("Child %d (pid %d) exiting.\n", processNum, getpid());
         return;
@@ -49,9 +52,13 @@ void pattern_two_child_process(int processNum, int numOfProcesses) {
         pattern_two_child_process(processNum + 1, numOfProcesses);
         exit(0);
     } else if (pId > 0) {
-        printf("Child %d (pid %d), sleeping %d seconds after creating child %d (pid %d) \n", processNum, getpid(), sleep_time, processNum + 1, pId);
+        printf(
+            "Child %d (pid %d), sleeping %d seconds after creating child %d "
+            "(pid %d) \n",
+            processNum, getpid(), sleep_time, processNum + 1, pId);
         sleep(sleep_time);
-        printf("Child %d (pid %d), waiting for child %d (pid %d) \n", processNum, getpid(), processNum + 1, pId);
+        printf("Child %d (pid %d), waiting for child %d (pid %d) \n",
+               processNum, getpid(), processNum + 1, pId);
         waitpid(pId, NULL, 0);
         // wait(NULL);
         printf("Child %d (pid %d) exiting.\n", processNum, getpid());
@@ -59,7 +66,6 @@ void pattern_two_child_process(int processNum, int numOfProcesses) {
         perror("Fork failed");
         exit(1);
     }
-    
 }
 
 void pattern_two_fork(int numOfProcesses) {
@@ -79,102 +85,3 @@ void pattern_two_fork(int numOfProcesses) {
         exit(1);
     }
 }
-
-// void pattern_three_child_process(int numOfProcesses) {
-//     printf("Child %d (pid %d) of %d: starting \n", curProcessNum, getpid(), numOfProcesses + 1);
-
-//     int sleep_time = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
-
-//     if (curProcessNum >= numOfProcesses) {
-//         printf("Child %d (pid %d), [no children created] sleeping %d seconds \n", curProcessNum, getpid(), sleep_time);
-//         sleep(sleep_time);
-//         printf("Child %d (pid %d) exiting.\n", curProcessNum, getpid());
-//         return;
-//     }
-
-//     bool isLeftAvailable = false;
-//     bool isRightAvailable = false;
-    
-//     if (curProcessNum + 2 <= numOfProcesses) {
-//         isLeftAvailable = true;
-//         isRightAvailable = true;
-//     } else if (curProcessNum + 1 <= numOfProcesses) {
-//         isLeftAvailable = true;
-//     }
-
-//     if (isLeftAvailable && isRightAvailable) {
-//         int pIdLeft = fork();
-        
-//         // left child
-//         if (pIdLeft == 0) {
-//             curProcessNum += 1;
-//             pattern_three_child_process(numOfProcesses);
-//             exit(0);
-//         } else if (pIdLeft > 0) {
-//             printf("Child %d (pid %d), sleeping %d seconds after creating child %d (pid %d) \n", curProcessNum - 1, getpid(), sleep_time, curProcessNum, pIdLeft);
-//             sleep(sleep_time);
-//             printf("Child %d (pid %d), waiting for child %d (pid %d) \n", curProcessNum - 1, getpid(), curProcessNum, pIdLeft);
-//             waitpid(pIdLeft, NULL, 0);
-//             printf("Child %d (pid %d) exiting.\n", curProcessNum - 1, getpid());
-//         } else {
-//             perror("Fork failed");
-//             exit(1);
-//         }
-
-//         // right child
-//         if (getpid() != pIdLeft) {
-//             int pIdRight = fork();
-
-//             if (pIdRight == 0) {
-//                 curProcessNum += 1;
-//                 pattern_three_child_process(numOfProcesses);
-//                 exit(0);
-//             } else if (pIdRight > 0) {
-//                 printf("Child %d (pid %d), sleeping %d seconds after creating child %d (pid %d) \n", curProcessNum - 1, getpid(), sleep_time, curProcessNum, pIdRight);
-//                 sleep(sleep_time);
-//                 printf("Child %d (pid %d), waiting for child %d (pid %d) \n", curProcessNum - 1, getpid(), curProcessNum, pIdRight);
-//                 waitpid(pIdRight, NULL, 0);
-//                 printf("Child %d (pid %d) exiting.\n", curProcessNum - 1, getpid());
-//             } else {
-//                 perror("Fork failed");
-//                 exit(1);
-//             }
-//         }  
-//     } else {
-//         int pIdLeft = fork();
-        
-//         // left child
-//         if (pIdLeft == 0) {
-//             curProcessNum += 1;
-//             pattern_three_child_process(numOfProcesses);
-//             exit(0);
-//         } else if (pIdLeft > 0) {
-//             printf("Child %d (pid %d), sleeping %d seconds after creating child %d (pid %d) \n", curProcessNum - 1, getpid(), sleep_time, curProcessNum, pIdLeft);
-//             sleep(sleep_time);
-//             printf("Child %d (pid %d), waiting for child %d (pid %d) \n", curProcessNum - 1, getpid(), curProcessNum, pIdLeft);
-//             waitpid(pIdLeft, NULL, 0);
-//             printf("Child %d (pid %d) exiting.\n", curProcessNum - 1, getpid());
-//         } else {
-//             perror("Fork failed");
-//             exit(1);
-//         }
-//     }
-// }
-
-// void pattern_three_fork(int numOfProcesses) {
-//     int curProcessNum = 0;
-//     int pId = fork();
-
-//     if (pId == 0) {
-//         pattern_three_child_process(numOfProcesses - 1);
-//         exit(0);
-//     } else if (pId > 0) {
-//         printf("Parent: created child %d (pid %d)\n", curProcessNum, pId);
-//         waitpid(pId, NULL, 0);
-//         printf("Parent: child %d (pid %d) has exited\n", curProcessNum, pId);
-//         printf("**Pattern 3: All children have exited\n");
-//     } else {
-//         perror("Fork failed");
-//         exit(1);
-//     }
-// }
--- forky.h
+++ forky.h
@@ -1,12 +1,12 @@
 #ifndef FORKY_H
 #define FORKY_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
-#include <stdbool.h>
+#include <unistd.h>
 
 #define MIN_VALUE 1
 #define MAX_VALUE 8
@@ -15,9 +15,5 @@ void pattern_one_child_process(int processNum);
 void pattern_one_fork(int numOfProcesses);
 void pattern_two_fork(int numOfProcesses);
 void pattern_two_child_process(int processNum, int numOfProcesses);
-// void pattern_three_fork(int numOfProcesses);
-// void pattern_three_child_process(int numOfProcesses);
-
-// static int curProcessNum;
 
 #endif
